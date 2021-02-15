0e46a4574d036026abee859c67ec926f08aceb58
abdel-bakadiri <abdelouahab.bakadiri@gmail.com>
2021-02-15 19:01:51 +0100

    Feat(): add a model

---
+++ src/main/java/tech/abdel/employeemanager/model/Employee.java
@@ -0,0 +1,19 @@
+package tech.abdel.employeemanager.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+}
